462767c3f7d4bab245b34d41bf6f7f4dd6529515
Lucas Gomes Santos <lgsantos306@gmail.com>
2023-07-02 19:31:40 -0300

    estrutura_inicial

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Avaliacoes from './pages/components/avaliacoes';
 import Banner from './pages/components/banner';
 import Cta from './pages/components/cta';
 import Footer from './pages/components/footer';
-import DestinosProcurados from './pages/components/maisProcurados';
 import Nav from './pages/components/navegacao';
 import Sobre from './pages/components/sobre';
 
@@ -12,7 +11,6 @@ function App() {
     <div>
       <Banner />
       <Nav />
-      <DestinosProcurados />
       <Sobre />
       <Avaliacoes />
       <Cta />
